54ca9f8aed95d6c007878c7c46a2ef033f202665
Ray <rainer.musch@gmx.de>
2024-12-19 14:24:02 +0100

    add html header und database.js

---
+++ script/database.js
@@ -0,0 +1,55 @@
+let meals = [
+    {
+      "pizza": [
+        {
+          "name": "Pizza Margherita",
+          "description": "beschreibung der Pizza",
+          "price": 6.00
+        },
+        {
+            "name": "Diavolo",
+            "description": "Hähnchen, Paprika, Zwiebeln",
+            "price": 7.90
+          },
+          {
+            "name": "BBQ",
+            "description": "Hackfleisch, Salami, Zwiebeln, Mais",
+            "price": 9.00
+          }
+      ],
+      "noodeles": [
+        {
+            "name": "Napoli",
+            "description": "mit Tomatensoße",
+            "price": 6.00
+          },
+          {
+              "name": "All Panna",
+              "description": "Schinken, Ei, Parmesan, Sahnesosse",
+              "price": 7.00
+            },
+            {
+              "name": "Carbonara",
+              "description": "Schinken und Sahnesoße",
+              "price": 9.00
+            }
+      ],
+      "burger": [
+        {
+            "name": "Hamburger",
+            "description": "Burgersosse, Salat, Gurke, Tomate",
+            "price": 6.00
+          },
+          {
+              "name": "Cheseburger",
+              "description": "Käse, Burgersosse, Salat, Gurke, Tomate",
+              "price": 7.00
+            },
+            {
+              "name": "XXL Hamburger",
+              "description": "2 X 180 Gramm Fleisch, Burgersosse, Salat, Gurke, Tomate",
+              "price": 9.90
+            }
+      ],
+    },
+]
