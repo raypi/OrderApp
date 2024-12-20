efea153b23e433c4000ceef8b58b401659385073
Ray <rainer.musch@gmx.de>
2024-12-20 10:20:28 +0100

    add amount to database

--- script/database.js
+++ script/database.js
@@ -4,51 +4,60 @@ let meals = [
         {
           "name": "Pizza Margherita",
           "description": "beschreibung der Pizza",
-          "price": 6.00
+          "price": 6.00,
+          "amount": 0
         },
         {
             "name": "Diavolo",
             "description": "Hähnchen, Paprika, Zwiebeln",
-            "price": 7.90
+            "price": 7.90,
+            "amount": 0
           },
           {
             "name": "BBQ",
             "description": "Hackfleisch, Salami, Zwiebeln, Mais",
-            "price": 9.00
+            "price": 9.00,
+            "amount": 0
           }
       ],
       "noodeles": [
         {
             "name": "Napoli",
             "description": "mit Tomatensoße",
-            "price": 6.00
+            "price": 6.00,
+            "amount": 0
           },
           {
               "name": "All Panna",
               "description": "Schinken, Ei, Parmesan, Sahnesosse",
-              "price": 7.00
+              "price": 7.00,
+              "amount": 0
             },
             {
               "name": "Carbonara",
               "description": "Schinken und Sahnesoße",
-              "price": 9.00
+              "price": 9.00,
+              "amount": 0
             }
       ],
       "burger": [
         {
             "name": "Hamburger",
             "description": "Burgersosse, Salat, Gurke, Tomate",
-            "price": 6.00
+            "price": 6.00,
+            "amount": 0
           },
           {
               "name": "Cheseburger",
               "description": "Käse, Burgersosse, Salat, Gurke, Tomate",
-              "price": 7.00
+              "price": 7.00,
+              "amount": 0
             },
             {
               "name": "XXL Hamburger",
               "description": "2 X 180 Gramm Fleisch, Burgersosse, Salat, Gurke, Tomate",
-              "price": 9.90
+              "price": 9.90,
+              "amount": 0
             }
       ],
     },
