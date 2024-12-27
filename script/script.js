d081b560a8667fea545d813cc972478dee0dacac
Ray <rainer.musch@gmx.de>
2024-12-27 10:18:23 +0100

    now removing the solved tasks in head of script

--- script/script.js
+++ script/script.js
@@ -1,8 +1,3 @@
-// Warenkorb rechts unter dem Header fix
-// Warenkorb bekommt Order BTN um Bestellung abzuschliessen, dann Pop Up mit Mitteilung
-// mindestens 3 Produkt Kategorien und 3 Produkte je Kategorie [Zusatzaufgabe: Verschiedene Reiter für verschiedene Produkte]
-// Jedes Produkt was in den Warenkorb kommt kann mit - oder Plus verändert werden, bei 1 und - verschwindet es aus dem Warenkorb
-// es gibt ein Löschen BTN für das Produkt im Warenbkorb
 // Bei der Handy APP rutsch der Warenkorb BTN nach Untern, der Angezeigte Warenkorb ist der gleich wie in der Desktop ansicht
 
 
